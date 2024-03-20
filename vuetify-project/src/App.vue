6901b88f6d0476b10ed928f27fb1308c0388d75d
alya0809 <azamatovaaltana@mail.ru>
2024-03-21 00:03:12 +0300

    Add anchore

--- vuetify-project/src/App.vue
+++ vuetify-project/src/App.vue
@@ -7,20 +7,14 @@
       </v-toolbar-title>
     </template>
     <v-app-bar-title>Сплитчек</v-app-bar-title>
-    <v-sheet id="section0" class="anchor-section">
-    </v-sheet>
+   
     <v-btn @click="$scrollTo('#section0')">Использовать</v-btn>
-    <v-sheet id="section1" class="anchor-section">
-    </v-sheet>
-    <v-btn @click="$scrollTo('#section1')">О продукте</v-btn>
-
-    <v-sheet id="section2" class="anchor-section">
-    </v-sheet>
-    <v-btn @click="$scrollTo('#section2')">Пример работы</v-btn>
-
-    <v-sheet id="section3" class="anchor-section">
-    </v-sheet>
-    <v-btn @click="$scrollTo('#section3')">Наша команда</v-btn>
+    <a href="#section1">
+      <v-btn>О продукте</v-btn>
+    </a>
+    <a href="#section2">
+      <v-btn>Пример работы</v-btn>
+    </a>
   </v-app-bar>
 
   <v-container class="text-center" style="margin-top: 64px;">
@@ -71,9 +65,7 @@
         <div style="position: relative; top: 100px; width: 80%; left: 11%;">
           <h2 style="font-size: 30px">Но все счастливы<br><br></h2>
           <p style="font-size: 25px;">Потому что знают про Сплитчек и быстро посчитают, на какую сумму пообедали.  Узнай и ты!</p>
-          <v-sheet id="section4" class="anchor-section">
-          </v-sheet>
-          <v-btn rounded="xs" size="x-large" block color="rgb(186, 104, 200)" style="margin-top: 10%;">Узнать подробнее</v-btn>
+          <a href="section1"><v-btn rounded="xs" size="x-large" block color="rgb(186, 104, 200)" style="margin-top: 10%;">Узнать подробнее</v-btn></a>
         </div>
       </v-col>
       <v-col cols="6">
@@ -81,27 +73,95 @@
       </v-col>
     </v-row>
   </v-container>
+  
+    <v-container>
+      <v-sheet id="section1" class="anchor-section">
+        <div style="max-width: 90%; margin-top: 20px">
+          <p style="font-size: 25px;">Сервис "Сплитчек" предназначен для удобного расчета стоимости блюд для каждого члена трапезы, если чек общий. Не нужно
+            регистрироваться, платить и покупать что-либо. Все, что требуется - доступ в интернет. Для того, чтобы получить результат, необходимо:<br>
+            1. Ввести всех гостей<br>
+            2. Ввести все позиции<br>
+            3. Нажать на кнопку "Получить расчет".
+          </p> 
+        </div>
+      </v-sheet> 
+    </v-container>
+  
   <v-container>
-    <div style="max-width: 90%;">
-      <p style="font-size: 25px;">Сервис "Сплитчек" предназначен для удобного расчета стоимости блюд для каждого члена трапезы, если чек общий. Не нужно
-        регистрироваться, платить и покупать что-либо. Все, что требуется - доступ в интернет. Для того, чтобы получить результат, необходимо:<br>
-        1. Ввести всех гостей<br>
-        2. Ввести все позиции<br>
-        3. Нажать на кнопку "Получить расчет".
-      </p>
-    </div>
+    <v-row>
+      <v-col cols="6">
+        <h2 style="font-size: 30px;">Всего 3 простых действия<br></h2>
+        <h2 style="font-size: 25px">И ты супер крут!</h2>
+        <div style="display: flex; align-items: flex-start; flex-wrap: wrap; flex-direction: column;">
+            <div style="display: flex; align-items: center; margin: 20px 0px;">
+                <img src="./components/logo/icon1.png" style="width: 50px; height: 50px; margin-right: 10px;" />
+                <p style="flex: 1; font-size: 22px;">Добавь гостей</p>
+            </div>
+            <div style="display: flex; align-items: center; margin: 20px 0px;">
+                <img src="./components/logo/icon2.png" style="width: 50px; height: 50px; margin-right: 10px;" />
+                <p style="flex: 1; font-size: 22px;">Добавь позиции</p>
+            </div>
+            <div style="display: flex; align-items: center; margin: 20px 0px;">
+                <img src="./components/logo/icon3.png" style="width: 50px; height: 50px; margin-right: 10px;" />
+                <p style="flex: 1; font-size: 22px;">Получи результат</p>
+            </div>
+        </div>
+
+      </v-col>
+      <v-col cols="6">
+        <img src="./components/logo/bro1.png" alt="Вторая картинка" style="max-width: 65%; margin-left: 15%;">
+      </v-col>
+    </v-row>
   </v-container>
+  <v-sheet id="section2" class="anchor-section">
+    <h2 class="text-center">Пример работы</h2>
+  </v-sheet>
+  <v-carousel hide-delimiters style="max-width: 800px; margin: 20px auto;">
+      <v-carousel-item
+      v-for="(photo, index) in photos"
+        :key="index"
+        :src="photo.src"
+        cover
+      ></v-carousel-item>
+  </v-carousel>
   
 </v-app>
 </template>
 
-<script setup>
-  //
+<script>
+  export default {
+    data: () => ({
+      photos: [
+        { src: 'https://www.sostav.ru/images/news/2019/04/01/9iithyjt_md.jpg', },
+        { src: 'https://admin.redmuseum.church/wp-content/uploads/2020/06/1-61.jpg',}, 
+        { src: 'https://takiedela.ru/wp-content/uploads/2024/03/4c338cf556d683b10e92791c86e7cb97.jpg'}
+      ],
+    }),
+    methods: {
+    scrollTo(elementId) {
+      const element = document.getElementById(elementId.substr(1));
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    },
+  },
+  }
 </script>
 
 <style>
   .center-button {
       margin: 0 auto; /* автоматическое выравнивание по горизонтали */
       color: rgb(186, 104, 200);
-}
+  }
+  .v-window {
+    max-width: 100%;
+    max-height: 100%;
+    width: 50%;
+    height: 100%;
+    margin: 0 auto;
+  }
+  .prim {
+
+  }
+
 </style>
